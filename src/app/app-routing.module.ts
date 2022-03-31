5dec798a12dce63834a2a82a5e6dc73686f8d2a1
mherrera 2020235 <88869102+MarckHerrera@users.noreply.github.com>
2022-03-31 16:33:55 -0600

    Marck
    Navbar, componente empresa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmpresasComponent } from './components/empresas/empresas.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'empresas', component: EmpresasComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { EmpresasComponent } from './components/empresas/empresas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    EmpresasComponent
   ],
   imports: [
     BrowserModule,
