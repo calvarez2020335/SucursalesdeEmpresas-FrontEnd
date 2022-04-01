7c0d069907a2db6c3a2a6ac3e9f5a571a0972ae5
mherrera 2020235 <88869102+MarckHerrera@users.noreply.github.com>
2022-03-31 19:03:09 -0600

    Marck
    Arreglos en botones y ya jala bien el html del agregar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { EmpresasComponent } from './components/empresas/empresas.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    EmpresasComponent
+    EmpresasComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
