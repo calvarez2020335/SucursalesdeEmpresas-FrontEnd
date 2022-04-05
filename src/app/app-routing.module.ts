a02175f593ff6dcaf6f321e731e5354f0efb36db
mherrera 2020235 <88869102+MarckHerrera@users.noreply.github.com>
2022-04-05 14:47:29 -0600

    Marck
    Todo lo del ejercicio sin arreglar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EmpresasComponent } from './components/empresas/empresas.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistrarComponent } from './components/registrar/registrar.component';
+import { SucursalesComponent } from './components/sucursales/sucursales.component';
 
 
 const routes: Routes = [
   { path: 'empresas', component: EmpresasComponent},
+  {path: '', component: InicioComponent},
+  {path: 'sucursales', component: SucursalesComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'registrar' , component: RegistrarComponent }
 
 ];
 
