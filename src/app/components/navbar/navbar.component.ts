8f502798b2c02d0307a0680527731208918ff785
mherrera 2020235 <88869102+MarckHerrera@users.noreply.github.com>
2022-04-07 01:25:07 -0600

    Marck
    Pagina de Inicio, arreglos Navbar, Cerrar Sesion, Registar con todos los datos

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,19 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import * as $ from "jquery";
+import { UsuarioService } from 'src/app/services/usuario.service';
 
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
+  providers: [UsuarioService]
 })
 export class NavbarComponent implements OnInit {
+  public token;
 
-  constructor() {
+  constructor(private _usuarioService: UsuarioService) {
+    this.token = this._usuarioService.getToken();
    }
 
   ngOnInit(): void {
   }
 
+  VaciarToken(){
+    this._usuarioService.clearToken()
+  }
 }
 
