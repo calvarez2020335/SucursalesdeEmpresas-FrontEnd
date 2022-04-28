52165e2b01d3db4d0225dd7082d7c37bbe3de3ad
Oscar Iván Cujcuj López <88805642+ocujcuj2020271@users.noreply.github.com>
2022-04-27 22:45:50 -0600

    Ver Producto por sucursal

--- src/app/components/productos-sucursales/productos-sucursales.component.ts
+++ src/app/components/productos-sucursales/productos-sucursales.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { SucursalesService } from 'src/app/services/sucursales.service';
+import { Sucursales } from '../../models/sucursales.model'
+import { UsuarioService } from 'src/app/services/usuario.service';
+import { ProductoSucursalService } from 'src/app/services/productoSucursal.service';
+import { ProductosSucursal } from 'src/app/models/productos.sucursales.model';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-productos-sucursales',
   templateUrl: './productos-sucursales.component.html',
-  styleUrls: ['./productos-sucursales.component.scss']
+  styleUrls: ['./productos-sucursales.component.scss'],
+  providers: [SucursalesService, UsuarioService, ProductoSucursalService]
 })
 export class ProductosSucursalesComponent implements OnInit {
 
-  constructor() { }
+  public productosModelGet: ProductosSucursal;
+  public sucursalesModelGetId: Sucursales;
+  public token;
+  constructor(
+    private _productosService: ProductoSucursalService,
+    public _sucursalesService: SucursalesService,
+    public _usuarioService: UsuarioService) {
 
-  ngOnInit(): void {
+    this.sucursalesModelGetId = new Sucursales('', '', '', '', 0, '');
+    this.token = this._usuarioService.getToken()
   }
 
+  ngOnInit(): void {
+    this.getProductoSucursal();
+
+  }
+
+  getProductoSucursal(){
+    this._productosService.ObtenerProductoSucursal(this.sucursalesModelGetId._id, this.token).subscribe(
+      (response)=>{
+        console.log(this.sucursalesModelGetId._id)
+        this.productosModelGet = response.ProductosSucursal;
+      },
+      (error)=>{
+        console.log(this.sucursalesModelGetId._id)
+        console.log(<any>error);
+      }
+    )
+  };
 }
